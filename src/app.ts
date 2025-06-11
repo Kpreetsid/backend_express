fd94adef59cb9c492bbc7b65f2c92de60f369e2a
Pawan Gautam <pawan.kumar@presageinsights.ai>
2025-06-11 12:00:11 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.use(morgan('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/', express.static(path.join(__dirname, '../uploadFiles')));
-
+// rate limit for request
 app.use(rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
