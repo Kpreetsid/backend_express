7a5fa52db4a44d0a32bfa9a1b14a9778a6a53ee4
Kamalpreet <k.preetsid@gmail.com>
2025-08-29 05:42:36 +0000

    Upload

--- src/configDB.ts
+++ src/configDB.ts
@@ -30,6 +30,7 @@ export const mailCredential = {
   secure: process.env.MAIL_SECURE === 'true',
   user: process.env.MAIL_USER!,
   pass: process.env.MAIL_PASS!,
+  from: process.env.MAIL_FROM!
 };
 
 export const externalAPI = {
