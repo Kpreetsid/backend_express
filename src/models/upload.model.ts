7f22a4b5bd7cd45c8ed86a31c4324fda3ef5919c
pkgtm2419 <pawangtm2419@gmail.com>
2025-09-12 18:22:23 +0530

    Work order create api updated

--- src/models/upload.model.ts
+++ src/models/upload.model.ts
@@ -23,23 +23,7 @@ export const uploadSchema = new Schema<IUpload>({
 }, {
   _id: false,
   timestamps: true,
-  versionKey: false,
-  toJSON: {
-    virtuals: true,
-    transform(doc: any, ret: any) {
-      ret.id = ret._id;
-      delete ret._id;
-      return ret;
-    }
-  },
-  toObject: {
-    virtuals: true,
-    transform: function (doc, ret) {
-      ret.id = ret._id;
-      delete ret._id;
-      return ret;
-    }
-  }
+  versionKey: false
 });
 
 export const UploadModel = mongoose.model<IUpload>('Schema_Upload', uploadSchema);
