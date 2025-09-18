5f867d4a07f708b2b2b4ac5b8d4eb42b640554ae
pkgtm2419 <pawangtm2419@gmail.com>
2025-09-18 16:20:55 +0530

    Upload

--- src/masters/team/team.service.ts
+++ src/masters/team/team.service.ts
@@ -6,7 +6,7 @@ import { IUser } from "../../models/user.model";
 export const getAll = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     try {
         const { account_id } = get(req, "user", {}) as IUser;
-        const match = { account_id: account_id, isActive: true };
+        const match = { account_id: account_id, visible: true };
         const data = await TeamsModel.find(match);
         if (!data || data.length === 0) {
             throw Object.assign(new Error('No data found'), { status: 404 });
@@ -23,7 +23,7 @@ export const getDataById = async (req: Request, res: Response, next: NextFunctio
             throw Object.assign(new Error('ID is required'), { status: 400 });
         }
         const { account_id } = get(req, "user", {}) as IUser;
-        const match = { account_id: account_id, _id: req.params.id, isActive: true };
+        const match = { account_id: account_id, _id: req.params.id, visible: true };
         const data = await TeamsModel.find(match);
         if (!data || data.length === 0) {
             throw Object.assign(new Error('No data found'), { status: 404 });
@@ -57,7 +57,7 @@ export const updateById = async (req: Request, res: Response, next: NextFunction
             throw Object.assign(new Error('ID is required'), { status: 400 });
         }
         const data = await TeamsModel.findByIdAndUpdate(id, body, { new: true });
-        if (!data || !data.isActive) {
+        if (!data || !data.visible) {
             throw Object.assign(new Error('No data found'), { status: 404 });
         }
         res.status(200).json({ status: true, message: "Data updated successfully", data });
@@ -72,10 +72,10 @@ export const removeById = async (req: Request, res: Response, next: NextFunction
             throw Object.assign(new Error('ID is required'), { status: 400 });
         }
         const data = await TeamsModel.findById(req.params.id);
-        if (!data || !data.isActive) {
+        if (!data || !data.visible) {
             throw Object.assign(new Error('No data found'), { status: 404 });
         }
-        await TeamsModel.findByIdAndUpdate(req.params.id, { isActive: false }, { new: true });
+        await TeamsModel.findByIdAndUpdate(req.params.id, { visible: false }, { new: true });
         res.status(200).json({ status: true, message: "Data deleted successfully" });
     } catch (error) {
         next(error);
