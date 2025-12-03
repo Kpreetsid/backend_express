46a4614d8f36ded6fa67a13207ccb7f08f305c7b
pkgtm2419 <pawangtm2419@gmail.com>
2025-12-03 18:49:02 +0530

    Upload

--- src/masters/feature/features.service.ts
+++
@@ -1,8 +0,0 @@
-import { readFile } from 'fs/promises';
-import path from 'path';
-
-export const getAllFeatures = async () => {
-  const dataPath = path.join(__dirname, '../../public/features.json');
-  const raw = await readFile(dataPath, 'utf-8');
-  return JSON.parse(raw);
-};
--- src/masters/formCategory/formCategory.service.ts
+++ src/masters/formCategory/formCategory.service.ts
@@ -1,24 +1,41 @@
 import { CategoryModel, ICategory } from "../../models/formCategory.model";
 import { IUser } from "../../models/user.model";
 
-export const getFormCategories = async (match: any): Promise<ICategory[]> => {
-  return await CategoryModel.find(match).sort({ _id: -1 });
-};
+class FormCategoryService {
 
-export const createFormCategory = async (body: any, user: IUser): Promise<ICategory | null> => {
-  const newCategoryBody = new CategoryModel({
-    account_id: user.account_id,
-    name: body.name,
-    description: body.description,
-    createdBy: user._id
-  });
-  return await newCategoryBody.save();
-};
+  async getFormCategories(match: any): Promise<ICategory[]> {
+    return CategoryModel.find(match).sort({ _id: -1 });
+  }
 
-export const updateById = async (id: string, body: any, user: IUser): Promise<ICategory | null> => {
-  return await CategoryModel.findByIdAndUpdate(id, { name: body.name, description: body.description, updatedBy: user._id }, { new: true });
-};
+  async getCategoryById(id: string, account_id: any): Promise<ICategory | null> {
+    return CategoryModel.findOne({ _id: id, account_id, visible: true });
+  }
 
-export const removeById = async (id: string): Promise<ICategory | null> => {
-  return await CategoryModel.findByIdAndUpdate(id, { visible: false }, { new: true });
-};
+  async categoryExists(account_id: any, name: string, excludeId?: string) {
+    const filter: any = { account_id, name, visible: true };
+    if (excludeId) {
+      filter._id = { $ne: excludeId };
+    }
+    return CategoryModel.findOne(filter);
+  }
+
+  async createFormCategory(body: any, user: IUser) {
+    const newCategory = new CategoryModel({
+      account_id: user.account_id,
+      name: body.name,
+      description: body.description || "",
+      createdBy: user._id
+    });
+    return newCategory.save();
+  }
+
+  async updateById(id: string, body: any, user: IUser) {
+    return CategoryModel.findByIdAndUpdate(id, { name: body.name, description: body.description, updatedBy: user._id }, { new: true } );
+  }
+
+  async removeById(id: string) {
+    return CategoryModel.findByIdAndUpdate(id, { visible: false }, { new: true });
+  }
+}
+
+export default new FormCategoryService();
