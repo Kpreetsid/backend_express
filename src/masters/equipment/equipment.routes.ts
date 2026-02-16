82d36aa5a070af5b19759ce7ccc436bd9789ef30
pkgtm2419 <pawangtm2419@gmail.com>
2026-02-16 18:38:21 +0530

    Upload

--- src/masters/equipment/equipment.routes.ts
+++ src/masters/equipment/equipment.routes.ts
@@ -10,8 +10,8 @@ export default (router: express.Router) => {
     equipmentRouter.get('/tree/:id', equipmentController.getAssetTreeById);
     equipmentRouter.get('/child/:id', equipmentController.getChildAsset);
     equipmentRouter.get('/make-copy/:id', hasRolePermission('asset', 'add_asset'), equipmentController.makeAssetCopy);
-    equipmentRouter.get('/:id', validateParamId, equipmentController.getAsset);
     equipmentRouter.post('/', hasRolePermission('asset', 'add_asset'), equipmentController.create);
+    equipmentRouter.get('/:id', validateParamId, equipmentController.getAsset);
     equipmentRouter.put('/:id', validateParamId, hasRolePermission('asset', 'edit_asset'), equipmentController.update);
     equipmentRouter.patch('/:id', validateParamId, hasRolePermission('asset', 'edit_asset'), equipmentController.updateAssetImage);
     equipmentRouter.delete('/:id', validateParamId, hasRolePermission('asset', 'delete_asset'), equipmentController.removeAsset);
